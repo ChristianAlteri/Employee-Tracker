ec1afde53bc07e2be823e520575ad256b1dc02c4
ChristianAlteri <alteri.christian@gmail.com>
2023-06-09 09:58:46 +0800

    addind logic to inquirer prompts

--- app.js
+++ app.js
@@ -13,69 +13,102 @@ ASCII = () => {
   console.log("| __|  \\/  | _ \\ |   / _ \\ \\ / / __| __| |_   _| _ \\ (_)_\\(_|(/ __| |/ /| __| _ \\ ");
   console.log("| _|| |\\/| |  _/ |__| (_) \\ V /| _|| _|    | | |   /  / _ \\  | (__  ' < | _||   / ");
   console.log("|___|_|  |_|_| |____|\\___/ |_| |___|___|   |_| |_|_\\ /_/ \\_\\  \\___|_|\\_\\|___|_|_\\");  
+  console.log(".......................................................................................");  
 }
-
 
 function start() {
   ASCII();
   inquirer
     .prompt([
       {
-        type: "input",
-        message:
-          "What is the name of your company (must be no more than 3 characters)?",
-        name: "company",
-        validate: validateCompany,
-      },
-      {
         type: "list",
-        message: "Select the shape of your logo:",
-        name: "shape",
-        choices: ["circle", "triangle", "square"],
-      },
-      {
-        type: "input",
-        message: "Select the color of your logo:",
-        name: "color",
-      },
-      {
-        type: "input",
-        message: "Select the color of your font:",
-        name: "textColor",
-      },
-      {
-        type: "input",
-        message: "Press y to make your SVG:",
-        name: "confirm",
+        message: "What would you like to do?",
+        name: "main",
+        choices: [
+          "View all Employees",
+          "Add Employee",
+          "Update Employee Role",
+          "View all Roles",
+          "Add Role",
+          "View all Departments",
+          "Add Departments",
+          "Quit",
+        ],
       },
     ])
-    // Store the data in response
-    .then((response) => {
-      if (response.confirm === "y") {
-        // De structure response
-        const { company, shape, color, textColor } = response;
-        // Store correct SVG data in logo by running toHTML()
-        let logo;
-        // Conditional checking what shape the user selected
-        if (shape === "triangle") {
-          const triangle = new Triangle(company, color, textColor);
-          logo = triangle.toHTML();
-        } else if (shape === "circle") {
-          const circle = new Circle(company, color, textColor);
-          logo = circle.toHTML();
-        } else if (shape === "square") {
-          const square = new Square(company, color, textColor);
-          logo = square.toHTML();
-        }
-        // write the logo to the folder ./examples
-        fs.writeFileSync("./examples/logo.svg", logo);
-        console.log("Generated logo.svg");
-      } else {
-        console.log("Let's try that again");
-        start();
+    .then(({ main }) => {
+      console.log(main);
+      if (main === "View all Employees") {
+        console.log("Logic for 'View all Employees' selected.");
+      } else if (main === "Add Employee") {
+        console.log("Logic for 'Add Employee' selected.");
+      } else if (main === "Update Employee Role") {
+        inquirer
+          .prompt([
+            {
+              type: "input",
+              message: "Which employee would you like to update?",
+              name: "employee",
+            },
+            {
+              type: "input",
+              message: "What is the new title of the role?",
+              name: "roleTitle",
+            },
+          ])
+          .then(({ employee, roleTitle }) => {
+            console.log("Employee:", employee);
+            console.log("Role Title:", roleTitle);
+            // Perform logic to update the employee role
+          });
+      } else if (main === "View all Roles") {
+        console.log("Logic for 'View all Roles' selected.");
+      } else if (main === "Add Role") {
+        console.log("Logic for 'Add Role' selected.");
+      } else if (main === "View all Departments") {
+        console.log("Logic for 'View all Departments' selected.");
+      } else if (main === "Add Departments") {
+        console.log("Logic for 'Add Departments' selected.");
+      } else if (main === "Quit") {
+        console.log("Logic for 'Quit' selected.");
       }
+      // You can access the selected option as 'main' variable here
+      // Perform further actions based on the selected option
     });
 }
+
+
+    // Store the data in response
+    //   if (response.confirm === "y") {
+    //     // De structure response
+    //     const { company, shape, color, textColor } = response;
+        // Store correct SVG data in logo by running toHTML()
+        // let logo;
+        // // Conditional checking what shape the user selected
+        // if (shape === "triangle") {
+        //   const triangle = new Triangle(company, color, textColor);
+        //   logo = triangle.toHTML();
+        // } else if (shape === "circle") {
+        //   const circle = new Circle(company, color, textColor);
+        //   logo = circle.toHTML();
+        // } else if (shape === "square") {
+        //   const square = new Square(company, color, textColor);
+        //   logo = square.toHTML();
+      //   }
+      //   // display the tables to the console using console.table
+      //   console.table([
+      //     {
+      //       name: 'foo',
+      //       age: 10
+      //     }, {
+      //       name: 'bar',
+      //       age: 20
+      //     }
+      //   ]);
+      // } else {
+      //   console.log("Let's try that again");
+      //   start();
+      // }
 
 start();
 
