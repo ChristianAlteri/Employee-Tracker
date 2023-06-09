0e7521e17d6739fd3a11372d13815c9f4d189557
ChristianAlteri <alteri.christian@gmail.com>
2023-06-09 09:10:51 +0800

    created inquirer boiler plate to populate

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+// Import dependencies
+const cTable = require('console.table');
+const mysql = require('mysql2/promise');
+const inquirer = require('inquirer');
+
+// Render title
 ASCII = () => {
   console.log("       *    (   (       )     )                 (                      )     (     ");
   console.log("     (  `   )\\ ))\\ ) ( /(  ( /(            *   ))\\ )   (       (    ( /(     )\\ )  ");
@@ -9,4 +15,70 @@ ASCII = () => {
   console.log("|___|_|  |_|_| |____|\\___/ |_| |___|___|   |_| |_|_\\ /_/ \\_\\  \\___|_|\\_\\|___|_|_\\");  
 }
 
-ASCII();
+
+function start() {
+  ASCII();
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message:
+          "What is the name of your company (must be no more than 3 characters)?",
+        name: "company",
+        validate: validateCompany,
+      },
+      {
+        type: "list",
+        message: "Select the shape of your logo:",
+        name: "shape",
+        choices: ["circle", "triangle", "square"],
+      },
+      {
+        type: "input",
+        message: "Select the color of your logo:",
+        name: "color",
+      },
+      {
+        type: "input",
+        message: "Select the color of your font:",
+        name: "textColor",
+      },
+      {
+        type: "input",
+        message: "Press y to make your SVG:",
+        name: "confirm",
+      },
+    ])
+    // Store the data in response
+    .then((response) => {
+      if (response.confirm === "y") {
+        // De structure response
+        const { company, shape, color, textColor } = response;
+        // Store correct SVG data in logo by running toHTML()
+        let logo;
+        // Conditional checking what shape the user selected
+        if (shape === "triangle") {
+          const triangle = new Triangle(company, color, textColor);
+          logo = triangle.toHTML();
+        } else if (shape === "circle") {
+          const circle = new Circle(company, color, textColor);
+          logo = circle.toHTML();
+        } else if (shape === "square") {
+          const square = new Square(company, color, textColor);
+          logo = square.toHTML();
+        }
+        // write the logo to the folder ./examples
+        fs.writeFileSync("./examples/logo.svg", logo);
+        console.log("Generated logo.svg");
+      } else {
+        console.log("Let's try that again");
+        start();
+      }
+    });
+}
+
+start();
+
+
+
+
